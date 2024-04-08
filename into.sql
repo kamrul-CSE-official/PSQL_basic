f585e76385063fc865f1658a2e3f6b71204b18ad
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-04-08 16:51:01 +0600

    first commit

---
+++ into.sql
@@ -0,0 +1,17 @@
+-- Active: 1712571129820@@127.0.0.1@5432@test3
+-- #Create Database
+--CREATE DATABASE test3
+
+-- #Create table 
+-- CREATE TABLE student (
+--     student_id INT,
+--     first_name VARCHAR(50),
+--     last_name VARCHAR(50),
+--     cgpa NUMERIC(1,2)
+-- )
+
+-- #Update a table name
+-- ALTER TABLE student RENAME TO learners;
+
+-- #Delete a table
+-- DROP TABLE learners;
