722a8dbf6d10ccabd1c813552761843389630b39
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-04-09 16:53:32 +0600

    update

--- into.sql
+++ into.sql
@@ -15,3 +15,28 @@ ALTER TABLE student RENAME TO learners;
 
 -- #Delete a table
 DROP TABLE learners;
+
+-- #Create a table with CONSTRAINTS
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    age INT DEFAULT 18
+)
+
+-- #Insert value
+INSERT INTO users VALUES(1, 'ABC', 'abc@gmail.com')
+
+-- # See all data form table
+SELECT * from users
+
+-- # See a single col in table 
+SELECT age from users
+
+-- #Create a table
+CREATE TABLE user1 (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    UNIQUE(username, email)
+);
